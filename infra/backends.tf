d637514b3890c401f8dd7c52f19cf1cbda28b265
Mark Chmarny <mark@chmarny.com>
2022-01-06 13:37:14 -0800

    terraform update

--- infra/backends.tf
+++ infra/backends.tf
@@ -2,7 +2,7 @@ terraform {
   backend "gcs" {
     # GCS bucket where terrafrom state will be saved
     # Must exists before 1st terraform init
-    bucket = "cloudy-labs-terraform-state"
+    bucket = "cloudylab-terraform-state"
     prefix = "prod"
   }
 }
